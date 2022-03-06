c9f91afd5a4fe44b0f23b301a435f62c4cdd85c2
leonarliu <leonarliu@tencent.com>
2022-03-06 14:52:43 +0800

    condisions and flow control

--- hello/hello.go
+++ hello/hello.go
@@ -7,5 +7,34 @@ import (
 )
 
 func Hello(req *restfulv3.Request, rep *restfulv3.Response) {
-	fmt.Fprintln(rep.ResponseWriter, "Hello world")
+	// name := req.QueryParameter("name")
+	// if name == "leo" {
+	// 	name = "Leo"
+	// } else if name == "lora" {
+	// 	name = "Lora"
+	// } else {
+	// 	name = "someone"
+	// }
+
+	// switch name {
+	// case "leo":
+	// 	name = "Leo"
+	// case "lora":
+	// 	name = "Lora"
+	// default:
+	// 	name = "someone"
+
+	// }
+	// response := fmt.Sprintf("Hello %s", name)
+
+	fmt.Println("this is head")
+	defer fmt.Println("this is defer func")
+	names := req.QueryParameters("name")
+
+	var response string
+	for i, name := range names {
+		response += fmt.Sprintf("Hello %d.%s \n", i+1, name)
+	}
+	fmt.Fprintln(rep.ResponseWriter, response)
+	fmt.Println("this is tail")
 }
